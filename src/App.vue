eed561b83fe5eaed325cb5d3b8e7f55c142d373c
mohit2314 <mohitpatel2314@gmail.com>
2022-03-13 23:30:15 +0530

    included google font & boxicons

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,7 @@
 </template>
 
 <style lang="scss">
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600&display=swap');
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;

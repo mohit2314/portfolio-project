b457edce69a90d29d9743d49d365910e76460b2d
Mohit Patel <mohitpatel2314@gmail.com>
2022-03-30 22:45:28 +0530

    modified about section

--- src/components/About.vue
+++ src/components/About.vue
@@ -4,7 +4,33 @@
     <h2 class="section__title">About Me</h2>
 
     <div class="about__container container grid">
-        <img src="" alt="" class="about__img">
+      <img src="../assets/img/home-img.jpg" alt="" class="about__img" />
+
+      <div class="about__data">
+        <div class="about__info">
+          <div class="about__box">
+            <i class="bx bx-award about__icon"></i>
+            <h3 class="about__title">Experience</h3>
+            <span class="about__subtitle">3+ Year Working</span>
+          </div>
+          <div class="about__box">
+            <i class="bx bx-briefcase-alt about__icon"></i>
+            <h3 class="about__title">Completed</h3>
+            <span class="about__subtitle">15 + Projects</span>
+          </div>
+          <div class="about__box">
+            <i class="bx bx-support about__icon"></i>
+            <h3 class="about__title">Support</h3>
+            <span class="about__subtitle">Online 24/7</span>
+          </div>
+        </div>
+
+        <p class="about__description">
+          Software Developer,who loves to build apps while paying great attention to performance, scalability, design pattern and maintainability."
+        </p>
+
+        
+      </div>
     </div>
   </section>
 </template>

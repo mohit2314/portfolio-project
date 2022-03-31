60f9c34a81646cf0324bfb787b245f2032fe8b3f
Mohit Patel <mohitpatel2314@gmail.com>
2022-03-31 23:28:15 +0530

    added scroll listener

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -8,6 +8,7 @@
 import Header from '@/components/Header.vue';
 import MainSection from '@/components/MainSection.vue';
 import About from '@/components/About.vue'
+// import main from '@/views/main'
 export default {
  components:{
    Header,
--- src/views/main.js
+++ src/views/main.js
@@ -1,16 +1,7 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import './registerServiceWorker'
-import router from './router'
-import store from './store'
-
-
 /*=============== CHANGE BACKGROUND HEADER ===============*/
 function scrollHeader(){
     const header = document.getElementById('header')
     // When the scroll is greater than 50 viewport height, add the scroll-header class to the header tag
-    // @ts-ignore
     if(this.scrollY >= 50) header.classList.add('scroll-header'); else header.classList.remove('scroll-header')
 }
 window.addEventListener('scroll', scrollHeader)
-createApp(App).use(store).use(router).mount('#app')

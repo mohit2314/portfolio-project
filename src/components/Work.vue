c0e1e2d72ded5b64fe1ada8b823678ac1de8642a
Mohit Patel <mohitpatel2314@gmail.com>
2022-04-06 01:02:47 +0530

    Added work images

--- src/components/Work.vue
+++ src/components/Work.vue
@@ -1,5 +1,23 @@
 <template>
-  <section class="work section" id="work"></section>
+  <section class="work section" id="work">
+      <span class="section__subtitle">
+          My Portfolio
+      </span>
+      <h2 class="section__title">Recent Works</h2>
+
+      <div class="work___filters">
+<span class="work__item">All</span>
+<span class="work__item">Web</span>
+<span class="work__item">Mobile</span>
+<span class="work__item">Design</span>
+      </div>
+
+      <div class="work__container container grid">
+          <div class="work__card">
+              <img src="assets/img/work1.jpg" alt="" class="work__img">
+          </div>
+      </div>
+  </section>
 </template>
 
 <script>

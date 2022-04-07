394f624ec16d23da0c4cab353b51b97ca68ff943
Mohit Patel <mohitpatel2314@gmail.com>
2022-04-08 00:19:36 +0530

    added testimonial images

--- src/components/Testimonials.vue
+++ src/components/Testimonials.vue
@@ -2,7 +2,17 @@
   <section class="testimonial section">
       <span class="section__subtitle">My clients say</span>
       <h2 class="section__title">Testimonial</h2>
-
+<div class="testimonial__container container">
+    <div>
+        <div class="testimonial__card">
+            <img src="../assets/img/testimonial1.png" alt="" class="testimonial__img">
+            <h3 class="testimonial__name">John Doe</h3>
+            <p class="testimonial__description">
+                A really good job,all aspects of the were followed step by step and with good results.
+            </p>
+        </div>
+    </div>
+</div>
   </section>
 </template>
 
